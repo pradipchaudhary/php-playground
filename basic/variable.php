9b15297361382f2624b9b541c23d9696adf16240
I'm Younz <iamyounz@gmail.com>
2024-03-06 02:04:58 +0545

    varibale...

---
+++ basic/variable.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * 
+ *      Variable
+ * ----------------------------------------------------------------
+ *      In PHP, variables are used to store and manipulate data.
+ * 
+ *      - Declare variables using the dollar sign ($) followed by the variable name.
+ *      - Variable names are case-sensitive.
+ *      - Start variable names with a letter or underscore.
+ * 
+ */
+
+
+
+$name = "Pradip";
+$NAME = "Younz";
+
+
+$_sayHello = "Say Hello";
+
+
+
+
+echo "$name";
+echo "$NAME";
+
+echo "$_sayHello <br />";
+
+
+
+
+
+?>
