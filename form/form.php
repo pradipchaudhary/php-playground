19795f0d42c5afe6810a39d34f26c276e176cade
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-03-08 05:17:03 +0545

    feat(UI): Enhance user interface layout and styling

--- form/form.php
+++ form/form.php
@@ -50,16 +50,21 @@ $formController->submitForm();
 
 <!-- Form -->
 <div class="container py-5">
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <div class="form-group">
-            <label for="name">Name:</label>
-            <input type="text" class="form-control" id="name" name="name" required>
+    <div class="row">
+        <div class="col-sm-5 offset-3">
+            <div class="card p-5">
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" name="email" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
 </div>
-
